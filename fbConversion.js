ad7f832d89342ee0cfa085729121542a574c170d
ShobhitVerma618 <103369695+ShobhitVerma618@users.noreply.github.com>
2023-10-05 12:05:38 +0530

    Update fbConversion.js

--- fbConversion.js
+++ fbConversion.js
@@ -8,7 +8,7 @@ const port = 3119; // You can change this to any port you prefer
 app.use(bodyParser.json());
 
 // Route for receiving webhook POST requests
-app.post('/recieveWebhook', (req, res) => {
+app.post('/Webhook2', (req, res) => {
   // Handle the incoming webhook data here
   const webhookData = req.body;
 
