d4f62d079866c28c1c2d74d344f3cc94b717bac8
ShobhitVerma618 <103369695+ShobhitVerma618@users.noreply.github.com>
2023-10-05 11:36:20 +0530

    Add files via upload

---
+++ fbConversion.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3119; // You can change this to any port you prefer
+
+// Use body-parser middleware to parse JSON request bodies
+app.use(bodyParser.json());
+
+// Route for receiving webhook POST requests
+app.post('/webhook', (req, res) => {
+  // Handle the incoming webhook data here
+  const webhookData = req.body;
+
+  // Process the webhook data as needed
+  console.log('Received webhook data:', webhookData);
+
+  // Send a response (optional)
+  res.status(200).json({ message: 'Webhook received successfully' });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
