de8c0a4eede4d766a21b15794fe409370e0bd7b6
ShobhitVerma618 <103369695+ShobhitVerma618@users.noreply.github.com>
2023-10-05 13:07:06 +0530

    Update fbConversion.js

--- fbConversion.js
+++ fbConversion.js
@@ -14,10 +14,19 @@ app.post('/Webhook2', (req, res) => {
 
   // Process the webhook data as needed
   console.log('Received webhook data:', webhookData);
-
+  if(webhookData.event.previous_data.status_type !== 'won'){
+    if(webhookData.event.data.status_type === 'won'){
+        moveAhead(webhookData.event.data)
+    }
+  }
   // Send a response (optional)
   res.status(200).json({ message: 'Webhook received successfully' });
 });
+
+//facebook event 
+function moveAhead(data){
+    console.log(data);
+}
 
 // Start the server
 app.listen(port, () => {
